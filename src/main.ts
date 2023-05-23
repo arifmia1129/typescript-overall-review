acaf17227e03b9102230d1db78d5dd1464de0a66
Md. Arif Mia <arif.vtti@gmail.com>
2023-05-23 06:42:15 +0600

    default and while cart import

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
-import { add as sum } from "./module";
+import * as Calculate from "./module";
+
+const res = Calculate.subtract(10, 5);
 
 const add = (param1:number, param2:number, param3:number):number => {
     return param1 + param2 + param3;
--- src/module.ts
+++ src/module.ts
@@ -6,3 +6,10 @@ export const add = (param1:number, param2:number):number => {
 export const subtract = (param1:number, param2:number)=>{
     return param1 - param2;
 }
+
+
+const multiply = (param1:number, param2:number) => {
+    return param1 *  param2;
+}
+
+export default multiply;
