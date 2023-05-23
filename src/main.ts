a21a38a7ff6b15b5efb5393a3d2d2e36ab1037c2
Md. Arif Mia <arif.vtti@gmail.com>
2023-05-23 06:29:09 +0600

    export & import

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import { add as sum } from "./module";
+
+const add = (param1:number, param2:number, param3:number):number => {
+    return param1 + param2 + param3;
+}
---
+++ src/module.ts
@@ -0,0 +1,8 @@
+export const add = (param1:number, param2:number):number => {
+    return param1 + param2;
+}
+
+
+export const subtract = (param1:number, param2:number)=>{
+    return param1 - param2;
+}
