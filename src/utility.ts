e19b446ebf93d7d126239ff8cfca09575fd0a307
Md. Arif Mia <arif.vtti@gmail.com>
2023-05-23 07:19:32 +0600

    Partial and Required utility types

--- src/utility.ts
+++ src/utility.ts
@@ -3,5 +3,21 @@ interface IPerson {
     age:number;
     phone:string;
     email:string;
-    gender:"male"|"female"|"other"
+    gender?:"male"|"female"|"other"
 }
+// Pick
+type Person = Pick<IPerson, "name" | "age" | "gender">;
+
+// Omit
+type Contact = Omit<IPerson, "name" | "age" | "gender">;
+
+
+
+// Partial -> to make all the properties be optional
+
+type OptionalType = Partial<IPerson>;
+
+
+// Required -> to make all the properties be required
+type RequiredType = Required<IPerson>;
+
